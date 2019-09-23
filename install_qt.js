753a2234c2bfd0c3ba7965bf7ca847d9c553fe39
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-09-23 22:55:00 +0200

    Rewrite install_qt.js to promise based flow

--- install_qt.js
+++ install_qt.js
@@ -1,45 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 const child_process = require('child_process');
 
 const installer_url = 'http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe';
 const installer_executable = 'qt-unified-windows-x86-online.exe';
 
-console.log('Downloading Installer');
-download(installer_url, installer_executable, downloaded);
+main();
 
-function downloaded() {
-  console.log('Downloading Completed\nStarting Installer');
-  child_process.execFileSync(installer_executable, ['--help'], { stdio: 'inherit' });
-  console.log('Installer Completed');
+function main() {
+  console.log('Downloading Installer');
+  downloadInstaller().then((response) => {
+    saveInstaller(response.data).then(() => {
+      console.log('Download Complete\nRunning Installer');
+      runInstaller();
+      // .then(() => {
+      //   console.log('Installer Completed Successfully!');
+      // }).catch((error) => {
+      //   console.log(error);
+      // })
+      ;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
 }
 
-function download (url, dest, cb) {
-    const file = fs.createWriteStream(dest);
-    const sendReq = request.get(url);
+function downloadInstaller() {
+  return axios.get(installer_url, {
+    responseType: 'stream'
+  });
+}
 
-    // verify response code
-    sendReq.on('response', (response) => {
-        if (response.statusCode !== 200) {
-            return cb('Response status was ' + response.statusCode);
-        }
+function saveInstaller(data) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(installer_executable);
+    data.pipe(file);
+    file.on("finish", resolve);
+    file.on("error", reject);
+  });
+}
 
-        sendReq.pipe(file);
-    });
-
-    // close() is async, call cb after close completes
-    file.on('finish', () => file.close(cb));
-
-    // check for request errors
-    sendReq.on('error', (err) => {
-        fs.unlink(dest);
-        return cb(err.message);
-    });
-
-    file.on('error', (err) => { // Handle errors
-        fs.unlink(dest); // Delete the file async. (But we don't check the result)
-        return cb(err.message);
-    });
-  }
+function runInstaller() {
+  child_process.execFileSync(installer_executable, ['--help'], {
+    stdio: 'inherit'
+  });
+  console.log('Installer Completed');
+}
