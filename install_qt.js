5a2a4f2d3902fc72706d86bb904f4900b09943a3
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-09-25 03:05:50 +0200

    Add support for multiple OSs

--- install_qt.js
+++ install_qt.js
@@ -2,25 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 const toolcache = require('@actions/tool-cache');
-
 const fs = require('fs');
 
 const installer_base_url = 'http://download.qt.io/official_releases/online_installers/';
 const installer_executables = {
-  'windows': 'qt-unified-windows-x86-online.exe',
+  'win32': 'qt-unified-windows-x86-online.exe',
   'linux': 'qt-unified-linux-x64-online.run',
-  'macintosh': 'qt-unified-mac-x64-online.dmg'
+  'darwin': 'qt-unified-mac-x64-online.dmg'
 };
 
-const installer_url = 'http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run';
+const installer_url = installer_base_url + installer_executables[process.platform];
 
 console.log('Downloading Installer');
 toolcache.downloadTool(installer_url)
 .then((installer)=>{
   console.log('Download Complete\nRunning Installer');
+
+  if (process.platform === 'linux') {
+    fs.chmodSync(installer, 755);
+  }
+
   return exec.exec(installer, ['--verbose', '--script', 'qt_installer_script.qs']);
 }).then(()=>{
   console.log('Installer Completed Successfully!');
-}).catch((error)=>{
-  console.log(error);
+}).catch((err)=>{
+  core.setFailed(`Install QT failed with error ${err}`);
 })
